74a7c8fad12d0cecba88789cf8327cf0fbce3150
JohnSwiftC <johnswift@live.com>
2025-07-20 18:47:10 -0500

    need rust 2024

--- lbclient/src/main.rs
+++ lbclient/src/main.rs
@@ -1,8 +1,8 @@
 use sqlite::Connection;
 fn main() {
-
+    let connection = load_db();
 }
 
 fn load_db() -> Connection {
-    
+    sqlite::open("client.db").expect("Unable to open local database...")
 }
