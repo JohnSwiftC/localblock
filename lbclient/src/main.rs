c91c5fe1b2cc3971cf8555cff8bb4646c6b22790
JohnSwiftC <johnswift@live.com>
2025-07-22 15:47:22 -0500

    start building out actual cli, might use clap

--- lbclient/src/main.rs
+++ lbclient/src/main.rs
@@ -1,6 +1,8 @@
 mod database;
 fn main() {
-    let connection = database::init_db_conn().unwrap();
-    database::create_private_key(&connection, "testkey").unwrap();
-    let key = database::load_signing_key(&connection, "testkey").unwrap();
+    let mut args = std::env::args();
+    let _name = args.next();
+    let command = args.next().expect("No args were provided!");
+
+    println!("{}", command);
 }
