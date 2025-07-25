172b7d45168da19c1141fdd51c9ab58070d4ecc5
JohnSwiftC <johnswift@live.com>
2025-07-24 19:45:38 -0500

    set up the beginning parts of the authority

--- lbauthority/src/main.rs
+++ lbauthority/src/main.rs
@@ -1,4 +1,24 @@
+use tokio::net::{TcpListener, TcpStream};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:2727").await.unwrap();
+    
+    loop {
+        let socket = match listener.accept().await {
+            Ok((s, _)) => s,
+            Err(e) => {
+                eprintln!("Failure on a client: {}", e); 
+                continue;    
+            }
+        };
+
+        tokio::spawn(async move {
+            handle_stream(socket).await
+        });
+    }
+}
+
+async fn handle_stream(socket: TcpStream) {
+    
 }
