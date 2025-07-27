600e53c6e0952528e00a4d1d997d7ec674621f71
JohnSwiftC <johnswift@live.com>
2025-07-28 01:17:35 +0200

    add sqlite and init db conn from client code

---
+++ lbauthority/src/database.rs
@@ -0,0 +1,39 @@
+use sqlite::Connection;
+use std::error::Error;
+
+
+pub fn init_db_conn(path: &str) -> Result<Connection, LoadingError> {
+    let conn = sqlite::open(path).map_err(|e| LoadingError::GenericSQLError {
+        message: format!("{}", e),
+    })?;
+    conn.execute("CREATE TABLE IF NOT EXISTS keys (name TEXT PRIMARY KEY, key BLOB)")
+        .map_err(|e| LoadingError::GenericSQLError {
+            message: format!("{}", e),
+        })?;
+    Ok(conn)
+}
+
+#[derive(Debug)]
+pub enum LoadingError {
+    NameNotFound,
+    KeyFailedLoad,
+    GenericSQLError { message: String },
+    GenericCryptoError { message: String },
+    GenericFileError { message: String },
+}
+
+impl std::fmt::Display for LoadingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LoadingError::NameNotFound => write!(f, "key is not present in the database"),
+            LoadingError::KeyFailedLoad => write!(
+                f,
+                "key is not properly formatted in the database, erase the entry and attempt to restore"
+            ),
+            LoadingError::GenericSQLError { message } => write!(f, "{}", message),
+            LoadingError::GenericCryptoError { message } => write!(f, "{}", message),
+            LoadingError::GenericFileError { message } => write!(f, "{}", message),
+        }
+    }
+}
+impl Error for LoadingError {}
--- lbauthority/src/main.rs
+++ lbauthority/src/main.rs
@@ -1,8 +1,12 @@
 use tokio::net::{TcpListener, TcpStream};
+use sqlite::Connection;
+
+mod database;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("0.0.0.0:2727").await.unwrap();
+    let connection = database::init_db_conn("auth.db").unwrap();
     
     loop {
         let socket = match listener.accept().await {
