9ae9f36cc159c3b061702a26dfe36ca5aad24402
JohnSwiftC <johnswift@live.com>
2025-07-28 01:30:21 +0200

    init db with current ledgers and history.

--- lbauthority/src/database.rs
+++ lbauthority/src/database.rs
@@ -1,12 +1,16 @@
 use sqlite::Connection;
 use std::error::Error;
 
-
 pub fn init_db_conn(path: &str) -> Result<Connection, LoadingError> {
     let conn = sqlite::open(path).map_err(|e| LoadingError::GenericSQLError {
         message: format!("{}", e),
     })?;
-    conn.execute("CREATE TABLE IF NOT EXISTS keys (name TEXT PRIMARY KEY, key BLOB)")
+    conn.execute("CREATE TABLE IF NOT EXISTS ledgers (key BLOB PRIMARY KEY, coins INTEGER")
+        .map_err(|e| LoadingError::GenericSQLError {
+            message: format!("{}", e),
+        })?;
+
+    conn.execute("CREATE TABLE IF NOT EXISTS history (transaction BLOB PRIMARY KEY)")
         .map_err(|e| LoadingError::GenericSQLError {
             message: format!("{}", e),
         })?;
--- lbauthority/src/main.rs
+++ lbauthority/src/main.rs
@@ -1,5 +1,5 @@
-use tokio::net::{TcpListener, TcpStream};
 use sqlite::Connection;
+use tokio::net::{TcpListener, TcpStream};
 
 mod database;
 
@@ -7,22 +7,18 @@ mod database;
 async fn main() {
     let listener = TcpListener::bind("0.0.0.0:2727").await.unwrap();
     let connection = database::init_db_conn("auth.db").unwrap();
-    
+
     loop {
         let socket = match listener.accept().await {
             Ok((s, _)) => s,
             Err(e) => {
-                eprintln!("Failure on a client: {}", e); 
-                continue;    
+                eprintln!("Failure on a client: {}", e);
+                continue;
             }
         };
 
-        tokio::spawn(async move {
-            handle_stream(socket).await
-        });
+        tokio::spawn(async move { handle_stream(socket).await });
     }
 }
 
-async fn handle_stream(socket: TcpStream) {
-    
-}
+async fn handle_stream(socket: TcpStream) {}
