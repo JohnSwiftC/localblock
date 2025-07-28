5a8221340a70f38ca5a9edda36d7321d283b144b
JohnSwiftC <johnswift@live.com>
2025-07-28 06:53:43 +0200

    transaction structures

--- lbauthority/lbcrypto/src/lib.rs
+++ lbauthority/lbcrypto/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use sha2::{Sha256, Digest};
+
+/// The representation of a block within a localblocl
+/// network. Similar to bitcoin's.
+pub struct Block {
+
 }
+
+type Txid = [u8; 32]; // Is just a sha256 hash
+type HashedPublic = [u8; 32];
+
+/// represents an input in a transaction
+/// references a previous transaction, and the index of the specific
+/// output that is trying to be spent
+struct TxInput {
+    txid: Txid,
+    index: u8,
+}
+
+struct TxOutput {
+    recip: HashedPublic,
+    amount: u32,
+}
+pub struct Transaction {
+    txid: Txid,
+    inputs: Vec<TxInput>,
+    output: Vec<TxOutput>,
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
